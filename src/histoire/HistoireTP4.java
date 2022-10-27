7956e7e50120895d9f6bd3e324f87ff928847f4e
darques mathilde <drm4255a@ens.fsi>
2022-10-27 14:53:23 +0200

    tp4

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,26 @@
+package histoire;
+
+import personnages.Humain;
+import personnages.Commercant;
+
+public class HistoireTP4 {
+	
+	
+	public static void main(String[] args) {
+		Humain Prof = new Humain("Prof","kombucha",54);
+		Prof.direBonjour();
+		Prof.acheter("boisson", 12);
+		Prof.boire();
+		Prof.acheter("jeu", 2);
+		Prof.acheter("kimono", 50);
+		
+		Commercant Marco = new Commercant("Marco",20);
+		Marco.direBonjour();
+		Marco.seFaireExtorquer();
+		Marco.recevoir(15);
+		Marco.boire();
+	}
+
+}
+
+
---
+++ src/personnages/Commercant.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Commercant extends Humain {
+
+	public Commercant(String nom, int argent) {
+		super(nom, "thé", argent);
+		
+	}
+
+	public int seFaireExtorquer() {
+		perdreArgent(getArgent());
+		parler("J'ai tout perdu! Le monde est trop injuste...");
+		return getArgent();
+	}
+	
+	public void recevoir(int benefice) {
+		gagnerArgent(benefice);
+		parler(benefice+" sous ! Je te remercie généreux donateur!");
+	}
+	
+}
